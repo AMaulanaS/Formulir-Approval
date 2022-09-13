6954e7629c91b528766ef77f180b00bb61ecfa49
Aku Alan Favoritmu <aditymaulino@gmail.com>
2022-09-13 08:56:30 +0700

    Delete config directory

--- config/routes.php
+++
@@ -1,6 +0,0 @@
-<?php
-defined('BASEPATH') or exit('No direct script access allowed');
-
-$route['default_controller'] = 'home';
-$route['404_override'] = '';
-$route['translate_uri_dashes'] = FALSE;
