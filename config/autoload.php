96ceb06d77fcbdffed1425ea3a3fedd61a0c3546
Aku Alan Favoritmu <aditymaulino@gmail.com>
2022-09-13 08:50:59 +0700

    Add files via upload

---
+++ config/autoload.php
@@ -0,0 +1,10 @@
+<?php
+defined('BASEPATH') or exit('No direct script access allowed');
+
+$autoload['packages'] = array();
+$autoload['libraries'] = array('database', 'session', 'form_validation');
+$autoload['drivers'] = array();
+$autoload['helper'] = array('url');
+$autoload['config'] = array();
+$autoload['language'] = array();
+$autoload['model'] = array();
